39db7d52c02a5554fadf916619e6933a932fbd81
stefian <v_ianta@yahoo.com>
2020-11-11 07:00:27 -0400

    + // customized statements //

--- test/app_test.js
+++ test/app_test.js
@@ -3,9 +3,9 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('The express app', () => {
-  it('handles a GET request to /api', () => {
+  it('handles a GET request to /api', (done) => {
     request(app)
-      .get('/api')
+      .get('/api')        // customized statements //
       .end((err, response) => {
 
       });

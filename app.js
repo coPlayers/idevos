f4c1bc6748a04854e36180a0bb86c432d108536f
stefian <v_ianta@yahoo.com>
2020-11-11 06:26:50 -0400

    + // Watch for incoming requests of method GET // // to the route http://localhost:3050/api // app.get('/api', (req, res) => {   res.send({ hi: 'there' }) });

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 
 const app = express();
 
+// Watch for incoming requests of method GET //
+// to the route http://localhost:3050/api //
 app.get('/api', (req, res) => {
   res.send({ hi: 'there' })
 });

d316f03d289bb9daced1cb2e7d5207fe135c1245
stefian <v_ianta@yahoo.com>
2020-11-12 09:52:31 -0400

    + mongoose.Promise = global.Promise; mongoose.connect('mongodb://localhost/idevos');

--- app.js
+++ app.js
@@ -1,7 +1,11 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const routes = require('./routes/routes');
 const app = express();
+
+mongoose.Promise = global.Promise;
+mongoose.connect('mongodb://localhost/idevos');
 
 app.use(bodyParser.json());
 routes(app);  // after app.use(bodyParser) //

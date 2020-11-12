88140fa3964459a6b546bdba37ca4eb5b885343f
stefian <v_ianta@yahoo.com>
2020-11-12 08:01:44 -0400

    +   it('Post to /api/devs creates a new dev', (done) => {     request(app)       .post('/api/devs')       .send({ email: 'test@test.com' })       .end(() => {         done();       });

--- test/controllers/devs_controller_test.js
+++ test/controllers/devs_controller_test.js
@@ -1,10 +1,16 @@
+const { doesNotMatch } = require('assert');
 const assert = require('assert');
 const request = require('supertest');
 const app = require('../../app');
 
 describe('Drivers controller', () => {
   //  Method - Route - Result = REST API it text convention //
-  it('Post to /api/devs creates a new dev', () => {
-
+  it('Post to /api/devs creates a new dev', (done) => {
+    request(app)
+      .post('/api/devs')
+      .send({ email: 'test@test.com' })
+      .end(() => {
+        done();
+      });
   });
 });

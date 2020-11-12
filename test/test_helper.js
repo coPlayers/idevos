cc54b3f704e5d35fdc98ed4e830d5f843b00cbbb
stefian <v_ianta@yahoo.com>
2020-11-12 12:28:53 -0400

    +   mongoose.connection     .once('open', () => done())     .on('error', err => {       console.warn('Warning', error);     });

--- test/test_helper.js
+++ test/test_helper.js
@@ -2,5 +2,9 @@ const mongoose = require('mongoose');
 
 before(done => {
   mongoose.connect('mongodb://localhost/idevos_test');
-  
+  mongoose.connection
+    .once('open', () => done())
+    .on('error', err => {
+      console.warn('Warning', error);
+    });
 });

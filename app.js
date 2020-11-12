dce2be9748bf5dec26074e40034545b646bf7c00
stefian <v_ianta@yahoo.com>
2020-11-12 12:09:32 -0400

    + if (process.env.NODE_ENV !== 'test') {   mongoose.connect('mongodb://localhost/idevos'); }  // else if testing - don't connect to db(dev) // // use the test/test_helper.js to connect to a test db //

--- app.js
+++ app.js
@@ -5,7 +5,10 @@ const routes = require('./routes/routes');
 const app = express();
 
 mongoose.Promise = global.Promise;
-mongoose.connect('mongodb://localhost/idevos');
+if (process.env.NODE_ENV !== 'test') {
+  mongoose.connect('mongodb://localhost/idevos');
+}  // else if testing - don't connect to db(dev) //
+// use the test/test_helper.js to connect to a test db //
 
 app.use(bodyParser.json());
 routes(app);  // after app.use(bodyParser) //

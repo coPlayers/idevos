f0f789d0caa4a9a4c815ab4887f7c98a90541d12
stefian <v_ianta@yahoo.com>
2020-11-12 06:13:36 -0400

    + const DevSchema = new Schema({   email: {     type: String,     required: true   },   coding: {     type: Boolean,     default: false   }, });

--- models/dev.js
+++ models/dev.js
@@ -2,5 +2,12 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const DevSchema = new Schema({
-
+  email: {
+    type: String,
+    required: true
+  },
+  coding: {
+    type: Boolean,
+    default: false
+  },
 });

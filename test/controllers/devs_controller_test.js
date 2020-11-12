3c4784659b7ca52b43ae3e0eaa3a51c9838c17da
stefian <v_ianta@yahoo.com>
2020-11-12 07:47:02 -0400

    + //  Method - Route - Result = REST API it text convention //

--- test/controllers/devs_controller_test.js
+++ test/controllers/devs_controller_test.js
@@ -3,6 +3,7 @@ const request = require('supertest');
 const app = require('../../app');
 
 describe('Drivers controller', () => {
+  //  Method - Route - Result = REST API it text convention //
   it('Post to /api/devs creates a new dev', () => {
 
   });

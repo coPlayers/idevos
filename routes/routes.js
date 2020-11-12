997d96dbac19a0a7e20261376bcf84aaea5c1e4d
stefian <v_ianta@yahoo.com>
2020-11-12 06:32:01 -0400

    + app.post('/api/devs', DevsController.create);

--- routes/routes.js
+++ routes/routes.js
@@ -4,4 +4,6 @@ module.exports = (app) => {
   // Watch for incoming requests of method GET //
   // to the route http://localhost:3050/api //
   app.get('/api', DevsController.greeting);
+
+  app.post('/api/devs', DevsController.create);
 };

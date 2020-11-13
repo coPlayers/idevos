97e9645966d3cf1ac5a4232eea82b9cb1ec2b61b
stefian <v_ianta@yahoo.com>
2020-11-12 20:57:16 -0400

    + if (process.env.NODE_ENV !== 'test') {   mongoose.connect('mongodb://localhost/idevos',     {       useNewUrlParser: true,       useUnifiedTopology: true     }   );

--- app.js
+++ app.js
@@ -6,7 +6,12 @@ const app = express();
 
 mongoose.Promise = global.Promise;
 if (process.env.NODE_ENV !== 'test') {
-  mongoose.connect('mongodb://localhost/idevos');
+  mongoose.connect('mongodb://localhost/idevos',
+    {
+      useNewUrlParser: true,
+      useUnifiedTopology: true
+    }
+  );
 }  // else if testing - don't connect to db(dev) //
 // use the test/test_helper.js to connect to a test db //
 
@@ -14,8 +19,8 @@ app.use(bodyParser.json());
 routes(app);  // after app.use(bodyParser) //
 
 // Middleware / Error handler //
-app.use(() => {
-
+app.use((err, req, res, next) => {
+  console.log(err);
 });
 
 module.exports = app;

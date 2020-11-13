f31e3a8576851710d0d3ef4f859206770e9b6ad8
stefian <v_ianta@yahoo.com>
2020-11-13 16:08:30 -0400

    + const PointSchema = new Schema({   type: { type: String, default: 'Point' },   coordinates: { type: [Number], index: '2dsphere' } });

--- models/dev.js
+++ models/dev.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const PointSchema = new Schema({
-
+  type: { type: String, default: 'Point' },
+  coordinates: { type: [Number], index: '2dsphere' }
 });
 
 const DevSchema = new Schema({
